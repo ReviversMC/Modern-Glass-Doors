b03abbc2f30009214a77c29add2754391f2e0070
Julian Burner <burnerjulian@gmail.com>
2022-05-14 16:33:03 +0200

    Remap some IDs

--- src/main/java/net/kyrptonaught/modern_glass_doors/ModernGlassDoorsMod.java
+++ src/main/java/net/kyrptonaught/modern_glass_doors/ModernGlassDoorsMod.java
@@ -10,81 +10,81 @@ import net.minecraft.client.render.RenderLayer;
 public class ModernGlassDoorsMod implements ModInitializer, ClientModInitializer {
     public static final String MOD_ID = "modern_glass_doors";
 
-    public static BlockGlassDoor oak_glassdoor;
-    public static BlockGlassDoor spruce_glassdoor;
-    public static BlockGlassDoor birch_glassdoor;
-    public static BlockGlassDoor acacia_glassdoor;
-    public static BlockGlassDoor jungle_glassdoor;
-    public static BlockGlassDoor dark_oak_glassdoor;
-    public static BlockGlassDoor iron_glassdoor;
+    public static BlockGlassDoor oakGlassDoor;
+    public static BlockGlassDoor spruceGlassDoor;
+    public static BlockGlassDoor birchGlassDoor;
+    public static BlockGlassDoor acaciaGlassDoor;
+    public static BlockGlassDoor jungleGlassDoor;
+    public static BlockGlassDoor darkOakGlassDoor;
+    public static BlockGlassDoor ironGlassDoor;
 
-    public static BlockGlassTrapDoor oak_glasstrapdoor;
-    public static BlockGlassTrapDoor spruce_glasstrapdoor;
-    public static BlockGlassTrapDoor birch_glasstrapdoor;
-    public static BlockGlassTrapDoor acacia_glasstrapdoor;
-    public static BlockGlassTrapDoor jungle_glasstrapdoor;
-    public static BlockGlassTrapDoor dark_oak_glasstrapdoor;
-    public static BlockGlassTrapDoor iron_glasstrapdoor;
+    public static BlockGlassTrapDoor oakGlassTrapdoor;
+    public static BlockGlassTrapDoor spruceGlassTrapdoor;
+    public static BlockGlassTrapDoor birchGlassTrapdoor;
+    public static BlockGlassTrapDoor acaciaGlassTrapdoor;
+    public static BlockGlassTrapDoor jungleGlassTrapdoor;
+    public static BlockGlassTrapDoor darkOakGlassTrapdoor;
+    public static BlockGlassTrapDoor ironGlassTrapdoor;
 
     @Override
     public void onInitialize() {
-        oak_glassdoor = new BlockGlassDoor(Block.Settings.copy(Blocks.OAK_DOOR), "oak_glassdoor");
-        spruce_glassdoor = new BlockGlassDoor(Block.Settings.copy(Blocks.SPRUCE_DOOR), "spruce_glassdoor");
-        birch_glassdoor = new BlockGlassDoor(Block.Settings.copy(Blocks.BIRCH_DOOR), "birch_glassdoor");
-        acacia_glassdoor = new BlockGlassDoor(Block.Settings.copy(Blocks.ACACIA_DOOR), "acacia_glassdoor");
-        jungle_glassdoor = new BlockGlassDoor(Block.Settings.copy(Blocks.JUNGLE_DOOR), "jungle_glassdoor");
-        dark_oak_glassdoor = new BlockGlassDoor(Block.Settings.copy(Blocks.DARK_OAK_DOOR), "dark_oak_glassdoor");
-        iron_glassdoor = new BlockGlassDoor(Block.Settings.copy(Blocks.IRON_DOOR), "iron_glassdoor");
+        oakGlassDoor = new BlockGlassDoor(Block.Settings.copy(Blocks.OAK_DOOR), "oak_glass_door");
+        spruceGlassDoor = new BlockGlassDoor(Block.Settings.copy(Blocks.SPRUCE_DOOR), "spruce_glass_door");
+        birchGlassDoor = new BlockGlassDoor(Block.Settings.copy(Blocks.BIRCH_DOOR), "birch_glass_door");
+        acaciaGlassDoor = new BlockGlassDoor(Block.Settings.copy(Blocks.ACACIA_DOOR), "acacia_glass_door");
+        jungleGlassDoor = new BlockGlassDoor(Block.Settings.copy(Blocks.JUNGLE_DOOR), "jungle_glass_door");
+        darkOakGlassDoor = new BlockGlassDoor(Block.Settings.copy(Blocks.DARK_OAK_DOOR), "dark_oak_glass_door");
+        ironGlassDoor = new BlockGlassDoor(Block.Settings.copy(Blocks.IRON_DOOR), "iron_glass_door");
 
-        oak_glasstrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.OAK_TRAPDOOR), "oak_glasstrapdoor");
-        spruce_glasstrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.SPRUCE_TRAPDOOR), "spruce_glasstrapdoor");
-        birch_glasstrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.BIRCH_TRAPDOOR), "birch_glasstrapdoor");
-        acacia_glasstrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.ACACIA_TRAPDOOR), "acacia_glasstrapdoor");
-        jungle_glasstrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.JUNGLE_TRAPDOOR), "jungle_glasstrapdoor");
-        dark_oak_glasstrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.DARK_OAK_TRAPDOOR), "dark_oak_glasstrapdoor");
-        iron_glasstrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.IRON_TRAPDOOR), "iron_glasstrapdoor");
+        oakGlassTrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.OAK_TRAPDOOR), "oak_glass_trapdoor");
+        spruceGlassTrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.SPRUCE_TRAPDOOR), "spruce_glass_trapdoor");
+        birchGlassTrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.BIRCH_TRAPDOOR), "birch_glass_trapdoor");
+        acaciaGlassTrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.ACACIA_TRAPDOOR), "acacia_glass_trapdoor");
+        jungleGlassTrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.JUNGLE_TRAPDOOR), "jungle_glass_trapdoor");
+        darkOakGlassTrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.DARK_OAK_TRAPDOOR), "dark_oak_glass_trapdoor");
+        ironGlassTrapdoor = new BlockGlassTrapDoor(Block.Settings.copy(Blocks.IRON_TRAPDOOR), "iron_glass_trapdoor");
     }
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(oak_glassdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(spruce_glassdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(birch_glassdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(acacia_glassdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(jungle_glassdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(dark_oak_glassdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(iron_glassdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(oakGlassDoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(spruceGlassDoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(birchGlassDoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(acaciaGlassDoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(jungleGlassDoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(darkOakGlassDoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ironGlassDoor, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(oak_glasstrapdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(spruce_glasstrapdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(birch_glasstrapdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(acacia_glasstrapdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(jungle_glasstrapdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(dark_oak_glasstrapdoor, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(iron_glasstrapdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(oakGlassTrapdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(spruceGlassTrapdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(birchGlassTrapdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(acaciaGlassTrapdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(jungleGlassTrapdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(darkOakGlassTrapdoor, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ironGlassTrapdoor, RenderLayer.getCutout());
 
     }
     public static BlockState copyState(BlockState copyState) {
         if (!(copyState.getBlock() instanceof DoorBlock)) return copyState;
 
-        BlockState newState = oak_glassdoor.getDefaultState();
-        if (copyState.getBlock().equals(Blocks.JUNGLE_DOOR)) newState = jungle_glassdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.BIRCH_DOOR)) newState = birch_glassdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.SPRUCE_DOOR)) newState = spruce_glassdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.ACACIA_DOOR)) newState = acacia_glassdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.DARK_OAK_DOOR)) newState = dark_oak_glassdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.IRON_DOOR)) newState = iron_glassdoor.getDefaultState();
+        BlockState newState = oakGlassDoor.getDefaultState();
+        if (copyState.getBlock().equals(Blocks.JUNGLE_DOOR)) newState = jungleGlassDoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.BIRCH_DOOR)) newState = birchGlassDoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.SPRUCE_DOOR)) newState = spruceGlassDoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.ACACIA_DOOR)) newState = acaciaGlassDoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.DARK_OAK_DOOR)) newState = darkOakGlassDoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.IRON_DOOR)) newState = ironGlassDoor.getDefaultState();
         return newState.with(DoorBlock.FACING, copyState.get(DoorBlock.FACING)).with(DoorBlock.HINGE, copyState.get(DoorBlock.HINGE)).with(DoorBlock.OPEN, copyState.get(DoorBlock.OPEN));
     }
     public static BlockState copyTrapdoorState(BlockState copyState) {
         if (!(copyState.getBlock() instanceof TrapdoorBlock)) return copyState;
 
-        BlockState newState = oak_glasstrapdoor.getDefaultState();
-        if (copyState.getBlock().equals(Blocks.JUNGLE_TRAPDOOR)) newState = jungle_glasstrapdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.BIRCH_TRAPDOOR)) newState = birch_glasstrapdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.SPRUCE_TRAPDOOR)) newState = spruce_glasstrapdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.ACACIA_TRAPDOOR)) newState = acacia_glasstrapdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.DARK_OAK_TRAPDOOR)) newState = dark_oak_glasstrapdoor.getDefaultState();
-        else if (copyState.getBlock().equals(Blocks.IRON_TRAPDOOR)) newState = iron_glasstrapdoor.getDefaultState();
+        BlockState newState = oakGlassTrapdoor.getDefaultState();
+        if (copyState.getBlock().equals(Blocks.JUNGLE_TRAPDOOR)) newState = jungleGlassTrapdoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.BIRCH_TRAPDOOR)) newState = birchGlassTrapdoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.SPRUCE_TRAPDOOR)) newState = spruceGlassTrapdoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.ACACIA_TRAPDOOR)) newState = acaciaGlassTrapdoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.DARK_OAK_TRAPDOOR)) newState = darkOakGlassTrapdoor.getDefaultState();
+        else if (copyState.getBlock().equals(Blocks.IRON_TRAPDOOR)) newState = ironGlassTrapdoor.getDefaultState();
         return newState.with(TrapdoorBlock.FACING, copyState.get(TrapdoorBlock.FACING)).with(TrapdoorBlock.OPEN, copyState.get(TrapdoorBlock.OPEN)).with(TrapdoorBlock.HALF, copyState.get(TrapdoorBlock.HALF)).with(TrapdoorBlock.POWERED, copyState.get(TrapdoorBlock.POWERED)).with(TrapdoorBlock.WATERLOGGED, copyState.get(TrapdoorBlock.WATERLOGGED));
     }
 }
--- src/main/java/net/kyrptonaught/modern_glass_doors/mixin/DefaultedRegistryMixin.java
+++ src/main/java/net/kyrptonaught/modern_glass_doors/mixin/DefaultedRegistryMixin.java
@@ -10,11 +10,72 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(DefaultedRegistry.class)
 public class DefaultedRegistryMixin {
-    @ModifyVariable(at = @At("HEAD"), method = "get(Lnet/minecraft/util/Identifier;)Ljava/lang/Object;", ordinal = 0)
-    Identifier fixMissingFromRegistry(@Nullable Identifier id) {
-        if(id != null) {
-            if(id.getNamespace().equals("glassdoor")) return new Identifier(ModernGlassDoorsMod.MOD_ID, id.getPath());
-        }
-        return id;
-    }
+
+	@ModifyVariable(at = @At("HEAD"), method = "get(Lnet/minecraft/util/Identifier;)Ljava/lang/Object;", ordinal = 0)
+	Identifier fixMissingFromRegistry(@Nullable Identifier originalIdentifier) {
+		if (originalIdentifier == null) {
+			return null;
+		}
+
+		if (originalIdentifier.getNamespace().equals(ModernGlassDoorsMod.MOD_ID)
+				|| originalIdentifier.getNamespace().equals("glassdoor")) {
+
+			String newPath;
+			switch (originalIdentifier.getPath()) {
+				// Normal glass doors
+				case "oak_glassdoor":
+					newPath = "oak_glass_door";
+					break;
+				case "spruce_glassdoor":
+					newPath = "spruce_glass_door";
+					break;
+				case "birch_glassdoor":
+					newPath = "birch_glass_door";
+					break;
+				case "acacia_glassdoor":
+					newPath = "acacia_glass_door";
+					break;
+				case "jungle_glassdoor":
+					newPath = "jungle_glass_door";
+					break;
+				case "dark_oak_glassdoor":
+					newPath = "dark_oak_glass_door";
+					break;
+				case "iron_glassdoor":
+					newPath = "iron_glass_door";
+					break;
+
+				// Glass trapdoors
+				case "oak_glasstrapdoor":
+					newPath = "oak_glass_trapdoor";
+					break;
+				case "spruce_glasstrapdoor":
+					newPath = "spruce_glass_trapdoor";
+					break;
+				case "birch_glasstrapdoor":
+					newPath = "birch_glass_trapdoor";
+					break;
+				case "acacia_glasstrapdoor":
+					newPath = "acacia_glass_trapdoor";
+					break;
+				case "jungle_glasstrapdoor":
+					newPath = "jungle_glass_trapdoor";
+					break;
+				case "dark_oak_glasstrapdoor":
+					newPath = "dark_oak_glass_trapdoor";
+					break;
+				case "iron_glasstrapdoor":
+					newPath = "iron_glass_trapdoor";
+					break;
+
+				default:
+					newPath = originalIdentifier.getPath();
+			}
+			return new Identifier(ModernGlassDoorsMod.MOD_ID, newPath);
+
+		} else {
+			return originalIdentifier;
+		}
+
+	}
 }

9c6b723041092000a108d4bb0dca45428c7302ec
thefirethirteen <thefirethirteen@protonmail.com>
2023-05-18 18:58:55 +0300

    Update how the item group is built for 1.20-pre2

--- src/main/java/com/github/reviversmc/modern_glass_doors/ModernGlassDoors.java
+++ src/main/java/com/github/reviversmc/modern_glass_doors/ModernGlassDoors.java
@@ -4,6 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import com.github.reviversmc.modern_glass_doors.blocks.ModernGlassDoorsBlocks;
@@ -13,11 +18,14 @@ public class ModernGlassDoors implements ModInitializer {
 	public static final String MOD_ID = "modern_glass_doors";
 
 	// ItemGroup Declaration
-	public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier(MOD_ID, "blocks"))
-			.icon(() -> new ItemStack(ModernGlassDoorsBlocks.OAK_GLASS_DOOR.asItem()))
-			.build();
+	public static final RegistryKey<ItemGroup> ITEM_GROUP = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(MOD_ID, "blocks"));
 
 	@Override
 	public void onInitialize() {
+		Registry.register(Registries.ITEM_GROUP, ITEM_GROUP, FabricItemGroup.builder()
+				.displayName(Text.translatable("itemGroup." + MOD_ID + ".blocks"))
+				.icon(() -> new ItemStack(ModernGlassDoorsBlocks.OAK_GLASS_DOOR.asItem()))
+				.build()
+		);
 	}
 }
